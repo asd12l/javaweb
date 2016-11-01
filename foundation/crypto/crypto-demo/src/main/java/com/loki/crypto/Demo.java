a2fcb6d4d3244ef0fd2798ae1e4d047e146f1a1c
soe.yen@hotmail.com <soe.yen#hotmail.com>
2016-11-01 15:50:51 +0800

    crypto demo

---
+++ foundation/crypto/crypto-demo/src/main/java/com/loki/crypto/Demo.java
@@ -0,0 +1,62 @@
+package main.java.com.loki.crypto;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.io.UnsupportedEncodingException;
+/**
+ * Created by 16070617 on 2016/11/1.
+ */
+public class Demo {
+
+    private  static  final  char[] PASSWORD = "enfldsgbnlsngdlksdsgm".toCharArray();
+
+    private  static  final  byte[] SALT =
+            {
+                    (byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12,
+                    (byte) 0xde, (byte) 0x33, (byte) 0x10, (byte) 0x12,
+            };
+
+    private  static  String encrypt(String property) throws  GeneralSecurityException, UnsupportedEncodingException
+    {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
+        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+        pbeCipher.init(Cipher.ENCRYPT_MODE, key, new PBEParameterSpec(SALT, 20));
+        return  base64Encode(pbeCipher.doFinal(property.getBytes("UTF-8")));
+    }
+
+    private  static  String decrypt(String property) throws GeneralSecurityException, IOException
+    {
+SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+        SecretKey key = keyFactory.generateSecret(new PBEKeySpec(PASSWORD));
+        Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+        pbeCipher.init(Cipher.DECRYPT_MODE,  key, new PBEParameterSpec(SALT, 20) );
+        return  new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+    }
+
+    private static  String base64Encode(byte[] bytes)
+    {
+        return  new BASE64Encoder().encode(bytes);
+    }
+
+    private  static  byte[] base64Decode(String property) throws IOException
+    {
+        return  new BASE64Decoder().decodeBuffer(property);
+    }
+    public static void main(String[] args) throws  Exception {
+        String originalPassword = "secret";
+        System.out.println("Original password: " + originalPassword);
+        String encryptedPassword = encrypt(originalPassword);
+        System.out.println("Encrypted password: " + encryptedPassword);
+        String decryptedPassword = decrypt(encryptedPassword);
+        System.out.println("Decrypted password: " + decryptedPassword);
+    }
+}
